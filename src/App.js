a1331f96accf41e62373642c1d4f87fb779630fb
Mohamed Aissa Benfodda <rygaa@protonmail.com>
2021-10-27 14:27:45 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import socketIOClient from "socket.io-client";
+export const socket = socketIOClient('ws://api.hanasu.me', {
+  path: '/mysocket/'
+});
+
 function App() {
+  useEffect(() => {
+    socket.emit('connection', 0)
+    console.log('connection emitted');
+
+    socket.on('connected', (socketId) => {
+      console.log('connected with server as socket:', socketId)
+    })
+    
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello world!!!</p>
     </div>
   );
 }
